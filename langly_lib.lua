dced4c2939207c4b14c4eba68bac6c512afe9a94
Seth McCauley <sethmccauley@users.noreply.github.com>
2018-01-18 14:33:23 -0700

    Update langly_lib.lua

--- langly_lib.lua
+++ langly_lib.lua
@@ -104,36 +104,48 @@ function detect_enchanted_gear()
 	return enchanted_gear
 end
 
--- Item checked must be equipped for this to return any meaningful value.
-function is_enchant_ready(--[[name of item]]item)
+function has_charges(--[[name of item]]item)
 	local item_id, item = res.items:find(function(v) if v.name == item then return true end end)
 	local inventory = windower.ffxi.get_items()
-	local usable_bags = T{'inventory','wardrobe','wardrobe2','wardrobe3','wardrobe4'}
+	local bags = T{'inventory','safe','safe2','storage','satchel','locker','sack','case','wardrobe','wardrobe2','wardrobe3','wardrobe4'}
+	local itemdata = {}
 	
-	if item_id ~= 0 then
-		for i,v in pairs(inventory) do
-			if usable_bags:contains(i) then
-				for key,val in pairs(v) do
-					if type(val) == 'table' and val.id == item_id and val.extdata then
-						local extdata = extdata.decode(val)
-						if extdata.activation_time - extdata.next_use_time > item.cast_delay then
-							return true
-						else
-							return false
-						end
-					end
+	for i,v in pairs(inventory) do
+		if bags:contains(i) then
+			for key,val in pairs(v) do
+				if type(val) == 'table' and val.id == item_id then
+					itemdata = extdata.decode(val)
 				end
 			end
 		end
 	end
+	
+	if itemdata and itemdata.charges_remaining then
+		if itemdata.charges_remaining > 0 then
+			return true
+		end
+	end
+	return false
 end
 
-function has_enchant_charges(--[[name of item]]item)
+function is_enchant_ready(--[[name of item]]item)
 	local item_id, item = res.items:find(function(v) if v.name == item then return true end end)
-	local extdata = extdata.decode(item)
+	local inventory = windower.ffxi.get_items()
+	local usable_bags = T{'inventory','wardrobe','wardrobe2','wardrobe3','wardrobe4'}
+	local itemdata = {}
 	
-	if extdata then
-		if extdata.charges_remaining > 0 then
+	for i,v in pairs(inventory) do
+		if usable_bags:contains(i) then
+			for key,val in pairs(v) do
+				if type(val) == 'table' and val.id == item_id then
+					itemdata = extdata.decode(val)
+				end
+			end
+		end
+	end
+	
+	if itemdata and itemdata.charges_remaining then
+		if itemdata.activation_time - itemdata.next_use_time > item.cast_delay then
 			return true
 		end
 	end
